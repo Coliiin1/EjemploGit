1208580621d18d49776d3b2c745e7c665f569c2a
Coliiin1 <carlosdanielavilacolin@gmail.com>
2025-08-13 08:58:03 -0600

    Primer programa

---
+++ src/ejemplogit/EjemploGit.java
@@ -0,0 +1,17 @@
+package ejemplogit;
+
+/**
+ *
+ * @author coliiin1
+ */
+public class EjemploGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+    }
+    
+}
